43081a80996bb7919c0997143534fca0a010f060
Pauline-WN <ngurupauline0@gmail.com>
2024-11-08 11:39:03 +0300

    Add IncidentMap component placeholder for displaying incident locations on a map

---
+++ client/src/components/IncidentMap.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Component to display a map with incident locations
+/* Instructions to backend team: 
+Provide incident location data as "latitude" and "longitude" properties within each incident object. 
+Ensure this data is accurate and accessible in the /api/incidents endpoint.*/
+
+const IncidentMap = ({ incidents }) => {
+  return (
+    <div className="map-container w-full h-64 bg-gray-200">
+      <p>Map displaying incident locations will appear here.</p>
+
+      {/* Use latitude and longitude from the backend data to set map markers for each incident */}
+    </div>
+  );
+};
+
+export default IncidentMap;
