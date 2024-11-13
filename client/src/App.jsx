dd7c35b4983c6ef20b692ecbcad80320df41fc0d
Pauline-WN <ngurupauline0@gmail.com>
2024-11-14 01:06:20 +0300

    adds readme file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import CreateIncident from './pages/CreateIncident'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import CreateIncident from "./components/CreateIncident";
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import MyProfile from "./components/MyProfile";
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/create-incident" element={<CreateIncident />} />
-      </Routes>
+      <div className="flex flex-col h-screen">
+        <Header />
+        <div className="flex flex-1">
+          <Sidebar />
+          <div className="flex-1 overflow-auto p-4">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/create-incident" element={<CreateIncident />} /> {/* Updated path */}
+              <Route path="/profile" element={<MyProfile />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
