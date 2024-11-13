7a1c08a9f17565948d0c7d2adc83dcb630586861
Pauline-WN <ngurupauline0@gmail.com>
2024-11-13 12:42:10 +0300

    sets up routing in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CreateIncident from "./pages/CreateIncident";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import CreateIncident from './pages/CreateIncident'
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/create-incident" element={<CreateIncident />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
