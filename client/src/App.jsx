8f87ed0b25c85c7928b1da7687718a87b783caef
jeangathoni8 <jeangathoni39@gmail.com>
2024-11-15 18:49:02 +0300

    Refactor import statements in App component for consistency and clarity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,14 @@ import { Provider } from 'react-redux'; // Importing Provider for Redux
 import { store } from "./store/store"; // Importing the Redux store
 import HomePage from "./pages/HomePage";
 import CreateIncident from "./components/CreateIncident";
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
 import Footer from "./components/Footer";
-import AdminDashboard from './pages/AdminDashboard'; // Ensure AdminDashboard is imported
-import ContactPage from './pages/ContactPage'; // Ensure ContactPage is imported
-import ManageIncidents from './pages/ManageIncidents';
+import AdminDashboard from "./pages/AdminDashboard"; // Ensure AdminDashboard is imported
+import ContactPage from "./pages/ContactPage"; // Ensure ContactPage is imported
+import ManageIncidents from "./pages/ManageIncidents";
 import UserSidebar from "./components/UserSidebar";
 import AdminSidebar from "./components/AdminSidebar"; // Ensure Sidebar is imported
 
