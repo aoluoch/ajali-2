777428c4cca2cde3528c7bbfd8c76e383653995a
jeangathoni8 <jeangathoni39@gmail.com>
2024-11-18 11:23:06 +0300

    Add ProtectedRoute component for authentication; implement Layout component for consistent page structure; update App component to use context providers and protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,83 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CreateIncident from "./pages/CreateIncident";
-/*import Sidebar from './components/Sidebar';*/
-/*import Header from './components/Header';*/
-import Footer from "./components/Footer";
-import MyProfile from "./pages/MyProfile";
-import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
-import Admin from './components/Admin';
-import ContactForm from './pages/ContactPage'; // Import the ContactPage component
-import Settings from './pages/Settings'; // Import the Settings component
-import Notifications from './pages/Notifications';
-import ManageIncidents from './pages/ManageIncidents';
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { NotificationProvider } from './context/NotificationContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Layout from './components/Layout';
+import {
+  Admin,
+  ContactPage,
+  CreateIncident,
+  HomePage,
+  LandingPage,
+  Login,
+  ManageIncidents,
+  MyProfile,
+  Notifications,
+  Settings
+} from './pages';
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col h-screen">
-        {/*<Header />*/}
-        <div className="flex flex-1">
-         { /* <Sidebar /> */}
-          <div className="flex-1 overflow-auto p-4">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/create-incident" element={<CreateIncident />} />
-              <Route path="/profile" element={<MyProfile />} />
-              <Route path="/login" element={<Login />} /> {/* If login page is needed */}
-              <Route path="/landing" element={<LandingPage />} /> {/* If landing page is needed */}
-              <Route path="/admin" element={<Admin />} /> 
-              <Route path="/notifications" element={<Notifications />} />
-              <Route path="/manage-incidents" element={<ManageIncidents/>} />
-              
-              <Route path="/contact" element={<ContactForm />} />
-              <Route path="/settings" element={<Settings />} /> 
-              {/* <Route path="/footer" element={<Footer />} /> If footer is needed */}
-            </Routes>
-            <Footer />
-          </div>
-        </div>
-      </div>
+      <AuthProvider>
+        <NotificationProvider>
+          <Routes>
+            {/* Public routes */}
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/contact" element={<ContactPage />} />
+
+            {/* Protected routes */}
+            <Route path="/home" element={
+              <ProtectedRoute>
+                <Layout>
+                  <HomePage />
+                </Layout>
+              </ProtectedRoute>
+            } />
+            <Route path="/create-incident" element={
+              <ProtectedRoute>
+                <Layout>
+                  <CreateIncident />
+                </Layout>
+              </ProtectedRoute>
+            } />
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <Layout>
+                  <MyProfile />
+                </Layout>
+              </ProtectedRoute>
+            } />
+            <Route path="/admin" element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            } />
+            <Route path="/notifications" element={
+              <ProtectedRoute>
+                <Layout>
+                  <Notifications />
+                </Layout>
+              </ProtectedRoute>
+            } />
+            <Route path="/manage-incidents" element={
+              <ProtectedRoute>
+                <Layout>
+                  <ManageIncidents />
+                </Layout>
+              </ProtectedRoute>
+            } />
+            <Route path="/settings" element={
+              <ProtectedRoute>
+                <Layout>
+                  <Settings />
+                </Layout>
+              </ProtectedRoute>
+            } />
+          </Routes>
+        </NotificationProvider>
+      </AuthProvider>
     </Router>
   );
 }
